e000bbdbb8ddf631db70cadc16f15c33d5408d60
kauanAg <117299925+KauanAg-devs@users.noreply.github.com>
2023-11-01 22:17:13 -0300

    Add files via upload

---
+++ tp2/index.ts
@@ -0,0 +1,43 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import userModel from './models/userModel';
+
+const app = express();
+app.use(express.json());
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/mongo')
+ .then(()=>{
+
+app.post('/getUser', async (req,res)=>{
+    const { email, password } = req.body as userUI;
+    const foundUser = await userModel.findOne({email, password})
+    res.status(foundUser ? 200 : 404).json({message: foundUser ?? `account not found`})
+})
+  
+app.post('/post',async (req,res) =>{
+    const {email, password} = req.body as userUI
+    const createdUser = await userModel.create({email, password})
+    res.status(201).json({message: createdUser})
+})
+
+app.put('/put', async (req,res)=>{
+    const {email, password, newEmail, newPassword} = req.body as userUI&{newPassword: string, newEmail: string}
+    const updatedUser = await userModel.findOneAndUpdate({email, password}, {email: newEmail, password: newPassword})
+    res.json({ message: updatedUser })
+})
+  
+app.delete('/delete', async (req,res)=>{
+    const { email, password } = req.body as userUI;
+    const deletedUser = await userModel.findOneAndDelete({email, password})
+    res.json({message: deletedUser})
+})  
+
+app.use((req,res)=>{
+    res.status(404).json(`route doesn't exists.`)
+})
+
+app.listen(2800, ()=>console.log(`o servidor ta rodando, é nessa porta aqui ó: ${2800}`))
+
+})
---
+++ tp2/interfaces/userInterface.ts
@@ -0,0 +1,3 @@
+interface userUI {
+    email: string, password: string
+}
